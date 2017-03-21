5dc2001c015e04989b986414f023b58d005658d4
Tyler Newman <tnewman16@gmail.com>
2017-03-21 14:21:05 -0400

    Altered Tests

--- NormingPerformersAlarmClock/test/com/csci360/alarmclock/ControllerTest.java
+++ NormingPerformersAlarmClock/test/com/csci360/alarmclock/ControllerTest.java
@@ -27,12 +27,6 @@ public class ControllerTest {
     public static void tearDownClass() {
     }
 
-    @Test
-    public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
     /**
      * Test of startRadio method, of class Controller.
      */
