567dd47f0d40ae6eb984c94540a7fea3744d4e74
blainebillings <blaine.t.billings@gmail.com>
2017-03-21 13:39:05 -0400

    Add files via upload

--- NormingPerformersAlarmClock/src/com/csci360/alarmclock/Controller.java
+++ NormingPerformersAlarmClock/src/com/csci360/alarmclock/Controller.java
@@ -1,14 +1,67 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.csci360.alarmclock;
 
 /**
  *
- * @author ty
+ * @author Normin' Performers
  */
 public class Controller {
+    private Radio radio;
+    private Clock clock;
+    private Alarm alarm1;
+    private Alarm alarm2;
+    private int volume;
     
+    public Controller() {
+        radio = new Radio();
+        clock = new Clock();
+        alarm1 = new Alarm();
+        alarm2 = new Alarm();
+        volume = 0;
+    }
+    
+    public void startRadio() {
+        radio.startRadio();
+    }
+    
+    public void stopRadio() {
+        radio.stopRadio();
+    }
+    
+    public void startTune() {
+        
+    }
+    
+    public void stopTune() {
+        
+    }
+    
+    public void snooze() {
+        if (alarm1.getSounding() || alarm2.getSounding()) {
+            radio.stopRadio();
+        }
+        
+        if (alarm1.getSounding()) {
+            alarm1.snooze();
+        }
+        
+        if (alarm2. getSounding()) {
+            alarm2.snooze();
+        }
+    }
+    
+    public void switchTimeFormat() {
+        clock.switchTimeFormat();
+    }
+    
+    public void setTime(int h, int m) {
+        clock.setTime(h, m);
+    }
+    
+    public void setAlarm(int alarmNumber, int h, int m) {
+        if (alarmNumber == 1) {
+            alarm1.setAlarmTime(h, m);
+        } else if (alarmNumber == 2) {
+            alarm2.setAlarmTime(h, m);
+        }
+    }
 }
