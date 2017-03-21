77fe3e6968549912509d5c55e9e0c8f8541f4b59
Tyler Newman <tnewman16@gmail.com>
2017-03-21 12:25:21 -0400

    Added Radio Test

--- NormingPerformersAlarmClock/src/com/csci360/alarmclock/Radio.java
+++ NormingPerformersAlarmClock/src/com/csci360/alarmclock/Radio.java
@@ -11,20 +11,73 @@ package com.csci360.alarmclock;
  */
 public class Radio {
     
-    String radioType;
-    double fmStation;
-    int amStation;
+    static final int MIN_AM = 540;
+    static final int MAX_AM = 1600;
+    static final double MIN_FM = 88.0;
+    static final double MAX_FM = 108.0;
+        
+    private String radioType = "FM";    // either FM or AM
+    private double fmStation = 100.1;
+    private int amStation = 1000;
+    private boolean playing = false;
     
-    public Radio(String type) {
-        this.radioType = type;
+    public Radio() {}
+    
+    public void startRadio() {
+        this.playing = true;
     }
     
-    public void startRadio() {}
+    public void stopRadio() {
+        this.playing = false;
+    }
     
-    public void stopRadio() {}
+    public void incrementStation() {
+        if (this.radioType.equals("FM")) {
+            this.fmStation = this.fmStation + .2;
+            if (this.fmStation > Radio.MAX_FM)
+                this.fmStation -= (Radio.MAX_FM - Radio.MIN_FM);
+            
+        } else {
+            this.amStation = this.amStation + 10;
+            if (this.amStation > Radio.MAX_AM)
+                this.amStation -= (Radio.MAX_AM - Radio.MIN_AM + 10);
+        }
+    }
     
-    public void incrementStation() {}
+    public void decrementStation() {
+        if (this.radioType.equals("FM")) {
+            this.fmStation = this.fmStation - .2;
+            if (this.fmStation < Radio.MIN_FM)
+                this.fmStation += (Radio.MAX_FM - Radio.MIN_FM);
+            
+        } else {
+            this.amStation = this.amStation - 10;
+            if (this.amStation < Radio.MIN_AM)
+                this.amStation += (Radio.MAX_AM - Radio.MIN_AM + 10);
+        }
+    }
     
-    public void decrementStation() {}
+    public void switchType() {
+        if (this.radioType.equals("FM"))
+            this.radioType = "AM";
+        else
+            this.radioType = "FM";
+    }
+
+    public int getAmStation() {
+        return amStation;
+    }
+
+    public double getFmStation() {
+        return fmStation;
+    }
+
+    public String getRadioType() {
+        return radioType;
+    }
+
+    public boolean isPlaying() {
+        return playing;
+    }
     
 }
