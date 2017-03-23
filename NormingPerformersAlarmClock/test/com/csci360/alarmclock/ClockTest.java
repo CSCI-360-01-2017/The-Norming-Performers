0e137a8cff263fed5cd415dbe5e82073904c9055
blainebillings <blaine.t.billings@gmail.com>
2017-03-23 15:05:55 -0400

    Added getTimeTest

--- NormingPerformersAlarmClock/test/com/csci360/alarmclock/ClockTest.java
+++ NormingPerformersAlarmClock/test/com/csci360/alarmclock/ClockTest.java
@@ -59,33 +59,47 @@ public class ClockTest {
      * Test of getTime method, of class Clock.
      */
     @Test
-    public void testGetTime() {
-        System.out.println("getTime");
+    public void testGetTimeString() {
+        System.out.println("getTimeString");
         Clock instance = new Clock();
         
         // TESTING 12 HOUR TIME
         assertTrue(instance.twelveHourTime);
         instance.setTime(14, 25);
-        assertEquals("2:25 pm", instance.getTime());
+        assertEquals("2:25 pm", instance.getTimeString());
         instance.setTime(2, 25);
-        assertEquals("2:25 am", instance.getTime());
+        assertEquals("2:25 am", instance.getTimeString());
         instance.setTime(23, 59);
-        assertEquals("11:59 pm", instance.getTime());
+        assertEquals("11:59 pm", instance.getTimeString());
         instance.setTime(0, 0);
-        assertEquals("12:00 am", instance.getTime());
+        assertEquals("12:00 am", instance.getTimeString());
         
-        // TESTING 12 HOUR TIME
+        // TESTING 24 HOUR TIME
         instance.switchTimeFormat();
         assertFalse(instance.twelveHourTime);
         instance.setTime(14, 25);
-        assertEquals("14:25", instance.getTime());
+        assertEquals("14:25", instance.getTimeString());
         instance.setTime(2, 25);
-        assertEquals("02:25", instance.getTime());
+        assertEquals("02:25", instance.getTimeString());
         instance.setTime(23, 59);
-        assertEquals("23:59", instance.getTime());
+        assertEquals("23:59", instance.getTimeString());
         instance.setTime(0, 0);
-        assertEquals("00:00", instance.getTime());
+        assertEquals("00:00", instance.getTimeString());
         
+    }
+    
+    private void testGetTime() {
+        System.out.println("getTime");
+        Clock instance = new Clock();
+        int h = 14;
+        int m = 25;
+        instance.setTime(h, m);
+        int expResult = h;
+        int result = instance.getTime().getHours();
+        assertEquals(expResult, result);
+        expResult = m;
+        result = instance.getTime().getMinutes();
+        assertEquals(expResult, result);
     }
     
 }
