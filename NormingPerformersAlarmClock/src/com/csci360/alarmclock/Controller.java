47d6f0eacf18f2e3ad7fd2c80db19506462eb95c
Tyler Newman <tnewman16@gmail.com>
2017-03-23 15:20:51 -0400

    Fixed Controller Tests

--- NormingPerformersAlarmClock/src/com/csci360/alarmclock/Controller.java
+++ NormingPerformersAlarmClock/src/com/csci360/alarmclock/Controller.java
@@ -1,15 +1,23 @@
 package com.csci360.alarmclock;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  *
  * @author Normin' Performers
  */
 public class Controller {
+    
     private Radio radio;
     private Clock clock;
     private Alarm alarm1;
     private Alarm alarm2;
     private int volume;
+    private boolean tunePlaying;
+    
+    // Java Timer used to increment the time
+    private Timer timer;
     
     public Controller() {
         radio = new Radio();
@@ -17,7 +25,58 @@ public class Controller {
         alarm1 = new Alarm();
         alarm2 = new Alarm();
         volume = 0;
+        // Creates a new Timer object
+        timer = new Timer();
+        // Creates a new schedule for the timer to act
+        // every minute
+        timer.schedule(new CheckTimeTask(), 0, 10000);
     }
+    
+    /**
+    * Class used to continually check the alarm times against the clock time
+    */
+   class CheckTimeTask extends TimerTask {
+        /**
+         * Used to check the alarm times
+         */
+        @Override
+        public void run() {
+            // Increments the time
+            checkAlarms();
+        }
+   }
+   
+   public void checkAlarms() {
+       // checks whether alarm1 should go off
+       if (this.alarm1.getHours() == this.clock.getTime().getHours()
+           && this.alarm1.getMinutes() == this.clock.getTime().getMinutes()) {
+           // the alarm1 is at the alarm time, must sound alarm
+           if (this.alarm1.isRadioAlarm()) 
+               // the alarm is in radio mode, plays the radio
+               this.startRadio();
+           
+           else
+               // the alarm is in tune mode, plays the tune
+               this.startTune();
+           
+           this.alarm1.setSounding(true);
+       }
+       
+       // checks whether alarm2 should go off
+       if (this.alarm2.getHours() == this.clock.getTime().getHours()
+           && this.alarm2.getMinutes() == this.clock.getTime().getMinutes()) {
+           // the alarm2 is at the alarm time, must sound alarm
+           if (this.alarm2.isRadioAlarm())
+               // the alarm is in radio mode, plays the radio
+               this.startRadio();
+           
+           else
+               // the alarm is in tune mode, plays the tune
+               this.startTune();
+           
+           this.alarm2.setSounding(true);
+       }
+   }
     
     public void startRadio() {
         radio.startRadio();
@@ -28,24 +87,32 @@ public class Controller {
     }
     
     public void startTune() {
-        
+        this.tunePlaying = true;
     }
     
     public void stopTune() {
-        
+        this.tunePlaying = false;
+    }
+    
+    public boolean radioIsPlaying() {
+        return this.radio.isPlaying();
+    }
+    
+    public boolean tuneIsPlaying() {
+        return this.tunePlaying;
     }
     
     public void snooze() {
-        if (alarm1.isSounding() || alarm2.isSounding()) {
-            radio.stopRadio();
-        }
-        
-        if (alarm1.isSounding()) {
-            alarm1.snooze();
-        }
-        
-        if (alarm2. isSounding()) {
-            alarm2.snooze();
+        if (this.alarm1Sounding() || this.alarm2Sounding()) {
+            this.radio.stopRadio();
+            this.stopTune();
+            
+            if (this.alarm1Sounding()) {
+                alarm1.snooze();
+            }
+            if (this.alarm2Sounding()) {
+                alarm2.snooze();
+            }
         }
     }
     
@@ -66,5 +133,25 @@ public class Controller {
             alarm2.setMinutes(m);
             
         }
+    }
+
+    public Alarm getAlarm1() {
+        return alarm1;
+    }
+
+    public Alarm getAlarm2() {
+        return alarm2;
+    }
+
+    public Clock getClock() {
+        return clock;
+    }
+    
+    public boolean alarm1Sounding() {
+        return this.alarm1.isSounding();
+    }
+    
+    public boolean alarm2Sounding() {
+        return this.alarm2.isSounding();
     }
 }
