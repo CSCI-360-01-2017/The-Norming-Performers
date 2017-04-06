847a05716d9a36be38082e3f3601ab0009dade7e
Tyler Newman <tnewman16@gmail.com>
2017-04-06 15:20:56 -0400

    Added to Interface

--- NormingPerformersAlarmClock/src/FXMLDocumentController.java
+++ NormingPerformersAlarmClock/src/FXMLDocumentController.java
@@ -9,26 +9,33 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 
 /**
  *
  * @author ty
  */
 public class FXMLDocumentController implements Initializable {
-    
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+        
+    @FXML private ToggleButton onOffButton;
+    @FXML private Circle powerCircle;
+    @FXML private Text timeText;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        this.powerCircle.setFill(Color.LAWNGREEN);
+        this.timeText.setText("12:00 PM");
+    }
+    
+    @FXML
+    private void onOffClicked(ActionEvent event) {
+        if (this.onOffButton.isSelected())
+            this.powerCircle.setFill(Color.LAWNGREEN);
+        else
+            this.powerCircle.setFill(Color.RED);
+    }
     
 }
--- NormingPerformersAlarmClock/src/Main.java
+++ NormingPerformersAlarmClock/src/Main.java
@@ -4,11 +4,15 @@
  * and open the template in the editor.
  */
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -20,24 +24,15 @@ public class Main extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        try {
+            BorderPane page = (BorderPane) FXMLLoader.load(Main.class.getResource("FXMLDocument.fxml"));
+            Scene scene = new Scene(page);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Alarm Clock");
+            primaryStage.show();
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
